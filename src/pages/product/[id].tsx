6c50efa600483352fee94ff0802c4781cfeedc68
Êxodo Gurgel <exodowellis@gmail.com>
2022-10-26 20:53:02 -0300

    :sparkles: feat: Create product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,27 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
+
+import camisetaImg from '../../assets/camisa.png'
+import Image from "next/image";
+
 export default function Product() {
   return (
-    <h1>Product</h1>
+    <ProductContainer>
+      <ImageContainer>
+        <Image
+          src={camisetaImg}
+          alt=""
+          width={520}
+          height={480}
+          placeholder="blur" 
+          // blurDataURL={camisetaImg}
+        />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta Beyond the Limits</h1>
+        <span>R$ 79,90</span>
+        <p>Tempus fermentum eget lacus, quis ante. Potenti sit pharetra, ridiculus amet. Bibendum pretium arcu arcu eget viverra at metus donec hendrerit. Rhoncus, nunc, eu at ac. At massa, fermentum amet ornare cras tincidunt nunc tincidunt. Netus lorem nulla nulla mattis integer velit dictum proin nibh.</p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,70 @@
+import { styled } from "..";
+
+export const ProductContainer = styled('main', {
+  width: '100%',
+  maxWidth: 1232,
+  margin: '0 auto',
+  padding: '0 2rem',
+  minHeight: 656,
+
+  display: 'flex',
+  gap: '4.5rem',
+  alignItems: 'stretch'
+})
+
+export const ImageContainer = styled('div', {
+  background: 'linear-gradient(180deg, #1EA483 0%, #7465D4 100%)',
+  borderRadius: 8,
+  width: '100%',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  }
+})
+
+export const ProductDetails = styled('div', {
+  maxWidth: 520,
+  display: 'flex',
+  flexDirection: 'column',
+
+  h1: {
+    fontSize: '$2xl',
+    marginTop: '1.75rem',
+    color: '$gray300',
+  },
+
+  span: {
+    fontSize: '$2xl',
+    marginTop: '1rem',
+    color: '$green300'
+  },
+
+  p: {
+    marginTop: '2.5rem',
+    fontSize: '$md',
+    color: '$gray300',
+    lineHeight: 1.6,
+  },
+
+  button: {
+    marginTop: 'auto',
+    width: '100%',
+    padding: '1.25rem',
+    backgroundColor: '$green500',
+    border: 0,
+    borderRadius: 8,
+    color: '$white',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    fontSize: '1.125rem',
+    transition: 'background-color 0.2s', 
+
+    '&:hover': {
+      backgroundColor: '$green300',
+    }
+  }
+})
